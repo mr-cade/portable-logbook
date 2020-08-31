dcb91a54dafaa194ec56ba53ea9520201ee92fdb
mr-cade <cadelpetersen@gmail.com>
2020-08-31 11:25:06 -0600

    paths

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -13,8 +13,8 @@ module.exports = function(app) {
 }
 
     // res.send('Hi Hello How are ya')
-    // res.sendFile(path.join(__dirname, "../public/signup.html"));
-    res.sendFile(path.join("../public/signup.html"));
+    res.sendFile(path.join(__dirname, "./public/signup.html"));
+    // res.sendFile(path.join("./public/signup.html"));
   });
 
   app.get("/login", function(req, res) {
