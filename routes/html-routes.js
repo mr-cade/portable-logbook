fe24ab21534ba5f06eebf392370435ae09e86629
mr-cade <cadelpetersen@gmail.com>
2020-08-31 11:39:22 -0600

    paths

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -22,7 +22,7 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.sendFile(path.join(__dirname, "public/login.html"));
   });
 
   // Here we've add our isAuthenticated middleware to this route.
