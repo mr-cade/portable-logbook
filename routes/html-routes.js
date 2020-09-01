1384003d9d6d6f5baf824aea7b96b71e500d4008
mr-cade <cadelpetersen@gmail.com>
2020-09-01 12:59:43 -0600

    html paths

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -12,7 +12,7 @@ module.exports = function (app) {
             res.redirect("/members");
         }
         // res.send('Hi Hello How are ya')
-        res.sendFile(path.join(__dirname, "../Public", "index"));
+        res.sendFile(path.join(__dirname, "../Public", "index.html"));
     });
     // res.sendFile(path.join("./public/signup.html"));
 
@@ -22,7 +22,7 @@ module.exports = function (app) {
             res.redirect("/members");
         }
         // res.send('Hi Hello How are ya')
-        res.sendFile(path.join(__dirname, "../Public", "signup"));
+        res.sendFile(path.join(__dirname, "../Public", "signup.html"));
     });
 
     app.get("/login", function (req, res) {
@@ -30,14 +30,14 @@ module.exports = function (app) {
         if (req.user) {
             res.redirect("/members");
         }
-        res.sendFile(path.join(__dirname, "../Public", "login"));
+        res.sendFile(path.join(__dirname, "../Public", "login.html"));
 
     });
 
     // Here we've add our isAuthenticated middleware to this route.
     // If a user who is not logged in tries to access this route they will be redirected to the signup page
     app.get("/members", isAuthenticated, function (req, res) {
-        res.sendFile(path.join(__dirname, "../Public", "members"));
+        res.sendFile(path.join(__dirname, "../Public", "members.html"));
     });
 
 };
