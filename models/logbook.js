2fb3c64e677a23c3952b3a19ea03416210f4aee4
mr-cade <cadelpetersen@gmail.com>
2020-09-06 11:42:10 -0600

    handlebars outline

--- models/logbook.js
+++ models/logbook.js
@@ -9,6 +9,7 @@ module.exports = function (sequelize, DataTypes) {
     signalIn: DataTypes.INTEGER,
     signalOut: DataTypes.INTEGER,
     frequency: DataTypes.DECIMAL,
+    mode: DataTypes.STRING,
     contactName: DataTypes.STRING,
     contactLocation: DataTypes.STRING,
     contactNotes: DataTypes.STRING
