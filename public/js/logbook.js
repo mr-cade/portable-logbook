273e3b6f52018a4d6dc718ab261d7fc3fbaac032
mr-cade <cadelpetersen@gmail.com>
2020-09-07 18:07:33 -0600

    login turns to logout when logged in. need it to function as logout though

--- public/js/logbook.js
+++ public/js/logbook.js
@@ -1,8 +1,8 @@
 const $tableID = $('#table');
- const $BTN = $('#export-btn');
- const $EXPORT = $('#export');
+const $BTN = $('#export-btn');
+const $EXPORT = $('#export');
 
- const newTr = `
+const newTr = `
 <tr class="hide">
   <td class="pt-3-half" contenteditable="true">Example</td>
   <td class="pt-3-half" contenteditable="true">Example</td>
@@ -18,78 +18,77 @@ const $tableID = $('#table');
   </td>
 </tr>`;
 
- $('.table-add').on('click', 'i', () => {
+$('.table-add').on('click', 'i', () => {
 
-   const $clone = $tableID.find('tbody tr').last().clone(true).removeClass('hide table-line');
+  const $clone = $tableID.find('tbody tr').last().clone(true).removeClass('hide table-line');
 
-   if ($tableID.find('tbody tr').length === 0) {
+  if ($tableID.find('tbody tr').length === 0) {
 
-     $('tbody').append(newTr);
-   }
+    $('tbody').append(newTr);
+  }
 
-   $tableID.find('table').append($clone);
- });
+  $tableID.find('table').append($clone);
+});
 
- $tableID.on('click', '.table-remove', function () {
+$tableID.on('click', '.table-remove', function () {
 
-   $(this).parents('tr').detach();
- });
+  $(this).parents('tr').detach();
+});
 
- $tableID.on('click', '.table-up', function () {
+$tableID.on('click', '.table-up', function () {
 
-   const $row = $(this).parents('tr');
+  const $row = $(this).parents('tr');
 
-   if ($row.index() === 0) {
-     return;
-   }
+  if ($row.index() === 0) {
+    return;
+  }
 
-   $row.prev().before($row.get(0));
- });
+  $row.prev().before($row.get(0));
+});
 
- $tableID.on('click', '.table-down', function () {
+$tableID.on('click', '.table-down', function () {
 
-   const $row = $(this).parents('tr');
-   $row.next().after($row.get(0));
- });
+  const $row = $(this).parents('tr');
+  $row.next().after($row.get(0));
+});
 
- // A few jQuery helpers for exporting only
- jQuery.fn.pop = [].pop;
- jQuery.fn.shift = [].shift;
+// A few jQuery helpers for exporting only
+jQuery.fn.pop = [].pop;
+jQuery.fn.shift = [].shift;
 
- $BTN.on('click', () => {
+$BTN.on('click', () => {
 
-   const $rows = $tableID.find('tr:not(:hidden)');
-   const headers = [];
-   const data = [];
+  const $rows = $tableID.find('tr:not(:hidden)');
+  const headers = [];
+  const data = [];
 
-   // Get the headers (add special header logic here)
-   $($rows.shift()).find('th:not(:empty)').each(function () {
+  // Get the headers (add special header logic here)
+  $($rows.shift()).find('th:not(:empty)').each(function () {
 
-     headers.push($(this).text().toLowerCase());
-   });
-
-   // Turn all existing rows into a loopable array
-   $rows.each(function () {
-     const $td = $(this).find('td');
-     const h = {};
-
-     // Use the headers from earlier to name our hash keys
-     headers.forEach((header, i) => {
-
-       h[header] = $td.eq(i).text();
-     });
-
-     data.push(h);
-   });
-
-   // Output the result
-   $EXPORT.text(JSON.stringify(data));
- });
-
- // PAGINATION
- $(document).ready(function () {
-    $('#dataTable-1').DataTable();
-    $('.dataTables_length').addClass('bs-select');
+    headers.push($(this).text().toLowerCase());
   });
 
-  
+  // Turn all existing rows into a loopable array
+  $rows.each(function () {
+    const $td = $(this).find('td');
+    const h = {};
+
+    // Use the headers from earlier to name our hash keys
+    headers.forEach((header, i) => {
+
+      h[header] = $td.eq(i).text();
+    });
+
+    data.push(h);
+  });
+
+  // Output the result
+  $EXPORT.text(JSON.stringify(data));
+});
+
+// PAGINATION
+$(document).ready(function () {
+  $('#dataTable-1').DataTable();
+  $('.dataTables_length').addClass('bs-select');
+});
+
