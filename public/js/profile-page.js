b194d130b21b96d7413b059264538318510c4405
mr-cade <cadelpetersen@gmail.com>
2020-09-08 09:50:02 -0600

    dynamic login button

--- public/js/profile-page.js
+++ public/js/profile-page.js
@@ -97,17 +97,4 @@ $(document).ready(() => {
     $('.dataTables_length').addClass('bs-select');
   });
 
-  $(document).ready(function() {
-    // This file just does a GET request to figure out which user is logged in
-    // and updates the HTML on the page
-    $.get("/api/user_data").then(function(data) {
-      $(".nav-signup").text(data.email);
-    });
-    $.get("api/user_data").then(function(data){
-      if(data) {
-        $(".nav-login").text("Logout")
-    }
-    }
-    )
-  });
   
