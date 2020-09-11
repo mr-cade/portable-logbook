83d165d85b61259f960d5c072658dfe1551111cc
mr-cade <cadelpetersen@gmail.com>
2020-09-10 18:02:24 -0600

    comments

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -39,7 +39,6 @@ module.exports = function (app) {
     // Here we've add our isAuthenticated middleware to this route.
     // If a user who is not logged in tries to access this route they will be redirected to the signup page
     app.get("/members", function (req, res) {
-        // console.log("data:", db.logbook);
         db.logbook.findAll({}).then(function (dbLogbook) {
             console.log(dbLogbook);
             res.render("logbook", {logbook:dbLogbook});
