02c731eea08eb6089528e0b9646bcc65af23227c
mr-cade <cadelpetersen@gmail.com>
2020-09-21 06:19:59 -0600

    contact form built (not submitting - troubleshoot)

--- public/js/logbook.js
+++ public/js/logbook.js
@@ -2,7 +2,6 @@ const $tableID = $('#table');
 const $BTN = $('#export-btn');
 const $EXPORT = $('#export');
 
-
 const newTr = `
 <tr class="hide">
   <td class="pt-3-half" contenteditable="true">Example</td>
@@ -92,5 +91,58 @@ $BTN.on('click', () => {
 $(document).ready(function () {
   $('#dataTable-1').DataTable();
   $('.dataTables_length').addClass('bs-select');
+
+  const contactForm = $("form#contactForm")
+  const timeInput = $("#timeInput");
+  const callsignInput = $("#callsignInput");  
+  const rstRInput = $("#rstRInput");  
+  const rstSInput = $("#rstSInput");
+  const frequencyInput = $("#frequencyInput");
+  const modeInput = $("#modeInput");
+  const nameInput = $("#nameInput");
+  const locationInput = $("#locationInput");
+  const notesInput = $("#notesInput");
+
+  contactForm.on("submit", function(event) {
+    event.preventDefault();
+    console.log("submit button pressed")
+    var contactData = {
+      contactTime: timeInput.val().trim(),
+      callsign: callsignInput.val().trim(),
+      signalIn: rstRInput.val().trim(),
+      signalOut: rstSInput.val().trim(),
+      frequency: frequencyInput.val().trim(),
+      mde: modeInput.val().trim(),
+      contactName: nameInput.val().trim(),
+      contactLocation: locationInput.val().trim(),
+      contactNotes: notesInput.val().trim()
+    }
+
+    saveContact(contactData.contactTime, contactData.callsign, contactData.signalIn, contactData.signalOut, contactData.frequency, contactData.mde, contactData.contactName, contactData.contactLocation, contactData.contactNotes);
+  })
+  
+  function saveContact(contactTime, callsign, signalIn, signalOut, frequency, mde, contactName, contactLocation, contactNotes) {
+    $.post("/api/logbook", {
+      contactTime: contactTime,
+      callsign: callsign,
+      signalIn: signalIn,
+      signalOut: signalOut,
+      frequency: frequency,
+      mde: mde,
+      contactName: contactName,
+      contactLocation: contactLocation,
+      contactNotes: contactNotes
+    })
+      .then(function(data) {
+        window.location.replace("/members")
+      })
+      .catch(handleErr);
+  }
+  function handleErr(err) {
+    $("#alert .msg").text(err.responseJSON);
+    $("#alert").fadeIn(500);
+    alert("Error saving. Please try again.")
+    window.location.replace("/members"); 
+  }
 });
 
